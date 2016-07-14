88438eb344bf83803918bf8214cde63e74047458
Paul Richards <peedeerich@gmail.com>
2016-07-14 15:37:32 +0100

    Step 3. Add comment react class which is used within commentList to display comments with Author

--- scripts/comments.js
+++ scripts/comments.js
@@ -11,14 +11,14 @@ var CommentBox = React.createClass({
   }
 });
 
-
-// now add the next elements - commentlist and comment form which live inside commentBox
+// Adding some comments to the comment lisr
 
 var CommentList = React.createClass({
   render: function () {
     return (
       <div className="commentList">
-        Hello, how are you? I am commentList.
+        <Comment author="Mike Hunt">comment 1</Comment>
+        <Comment author="Ivor Biggen">commen 2</Comment>
       </div>
     );
   }
@@ -29,6 +29,21 @@ var CommentForm = React.createClass({
     return (
       <div className="commentForm">
         Yo. I am commentForm.
+      </div>
+    );
+  }
+});
+
+//Now add a comment React class which is used within comment list to display comments with their author
+
+var Comment = React.createClass({
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
