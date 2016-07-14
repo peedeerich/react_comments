db6e8ff4af95944196fed584f44e20c01fa303e9
Paul Richards <peedeerich@gmail.com>
2016-07-14 16:13:25 +0100

    Step 4. Linked to server and push data to commentList

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,24 +1,55 @@
+// data array has been removed now
+
+
+// add an initial state to the CommentBox, this is just an empty array because there are no comments
+
+
 
 var CommentBox = React.createClass({
+  getInitialState: function () {
+    return {data: []};
+  },
+
+  // add componentDidMount. This is called once the commentBox is successfully rendered for first time
+
+  componentDidMount: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString())
+      }.bind(this)
+    });
+  },
+
   render: function () {
     return (
       <div className="commentBox">
        <h1>Hello, world! I am a CommentBox.</h1>
         <CommentForm />
-        <CommentList />
+        <CommentList data={this.state.data} /> /*this.state.data is dynamic + can change, replaces props*/
       </div>
     );
   }
 });
 
-// Adding some comments to the comment lisr
 
 var CommentList = React.createClass({
   render: function () {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      )
+    });
     return (
       <div className="commentList">
-        <Comment author="Mike Hunt">comment 1</Comment>
-        <Comment author="Ivor Biggen">commen 2</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -34,7 +65,6 @@ var CommentForm = React.createClass({
   }
 });
 
-//Now add a comment React class which is used within comment list to display comments with their author
 
 var Comment = React.createClass({
   render: function () {
@@ -49,9 +79,10 @@ var Comment = React.createClass({
   }
 });
 
+// tell commentBox to get its data from a server
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox url="https://northcoders-comment-box-server.herokuapp.com/api/comments" />,
   document.getElementById('content')
 );
 
